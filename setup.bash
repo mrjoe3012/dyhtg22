4080f44d00a5e889042535bd2270035f540e6735
Joseph Agrane <2663460A@student.gla.ac.uk>
2022-10-14 20:36:31 +0100

    updated setup.bash

--- setup.bash
+++ setup.bash
@@ -2,11 +2,10 @@ echo INSTALLING NODEJS...
 cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
-sudo apt install nodejs
+sudo apt install -y nodejs
 echo ENABLING YARN...
 sudo corepack enable
 echo INSTALLING PYTHON3...
-sudo apt install python3
-sudo apt install python3.10-venv
+sudo apt install -y python3 python3.10-venv python3-pip
 echo FINISHED
 
