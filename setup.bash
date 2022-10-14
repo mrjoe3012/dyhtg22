4a215b637b0e5d5731643ebe762be992adb75cd4
Joseph Agrane <2663460A@student.gla.ac.uk>
2022-10-14 21:06:45 +0100

    modified setup

--- setup.bash
+++ setup.bash
@@ -1,11 +1,16 @@
+base_dir=$(pwd)
 echo INSTALLING NODEJS...
 cd ~
 curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
-sudo apt install -y nodejs npm
+sudo apt install -y nodejs
 echo ENABLING YARN...
 sudo corepack enable
 echo INSTALLING PYTHON3...
 sudo apt install -y python3 python3.10-venv python3-pip
+echo INSTALLING DEPENDENCIES
+cd ${base_dir}
+source api/venv/bin/activate
+pip install -r api/requirements.txt
 echo FINISHED
 
