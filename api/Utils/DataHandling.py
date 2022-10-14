4b063daaa6e1eeff27ea18d91f40dc520d55b7cd
gianmarco_crn <gianmarco.cornacchia@yahoo.it>
2022-10-15 00:32:43 +0100

    Added heatmap dictionary

--- api/Utils/DataHandling.py
+++ api/Utils/DataHandling.py
@@ -9,7 +9,24 @@ def get_security_logs(db_filename):
     return query_result
 
 def get_heatmap_values(security_logs):
-    # go through file
-    # count occurences of each location
-    # return a mapping of location names to occurences
-    pass #gianmarco
+    # python array of tuples, all containing strings with 
+    occurrence_dict = {"Boyd Orr Building": 0,
+                        "James Watt Building": 0,
+                        "Adam Smith Building": 0,
+                        "Main Building": 0,
+                        "Wolfson Medical Building": 0,
+                        "Glasgow University Union": 0,
+                        "The Hive": 0,
+                        "Sir Alwyn Williams Building": 0,
+                        "Library": 0,
+                        "Queen Margaret Union": 0,
+                        "St Andrews Building": 0,
+                        "Kelvingrove Park": 0,
+                        "Joseph Black Building": 0,
+                        "Kelvin Building" :0
+                        }
+    for tuple in get_security_logs("data/Data"):
+        if(tuple[2] in occurrence_dict.keys()):
+            occurrence_dict[tuple[2]] += 1
+
+    return occurrence_dict
