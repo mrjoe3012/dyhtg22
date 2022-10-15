fb055a609a6aca009fbff37ad70c57bb2865d66a
Jamie Robb <jamierobb321@gmail.com>
2022-10-15 14:06:50 +0100

    Add all buildings

--- src/components/Building.js
+++ src/components/Building.js
@@ -4,7 +4,7 @@ function Building(props)
 {
     return (
         <div className="Building">
-            <img src={props.image} className="building-image" style={{'filter': `opacity(${props.intensity}) drop-shadow(0 0 0 red) drop-shadow(0 0 0 red)`}}></img>
+            <img src={props.image} className="building-image" style={{'filter': `opacity(${props.intensity}) drop-shadow(0 0 0 red) drop-shadow(0 0 0 red) drop-shadow(0 0 0 red)`}}></img>
         </div>
     )
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -8,6 +8,18 @@ import Building from './Building';
 import mapBackground from '../assets/buildings/map_bottom.png';
 import boydOrrImage from '../assets/buildings/boydOrr.png';
 import libraryImage from '../assets/buildings/library.png';
+import stAndrewsImage from '../assets/buildings/stAndrews.png';
+import kelvinImage from '../assets/buildings/kelvin.png';
+import josephBlackImage from '../assets/buildings/josephBlack.png';
+import qmuImage from '../assets/buildings/qmu.png';
+import alwynImage from '../assets/buildings/sirAlwyn.png';
+import hiveImage from '../assets/buildings/hive.png';
+import wolfsonImage from '../assets/buildings/wolfson.png';
+import guuImage from '../assets/buildings/guu.png';
+import mainImage from '../assets/buildings/main.png';
+import adamSmithImage from '../assets/buildings/adamSmith.png';
+import jwsImage from '../assets/buildings/jws.png';
+import kelvingroveImage from '../assets/buildings/kelvingrove.png';
 
 function Map()
 {
@@ -27,7 +39,8 @@ function Map()
 
     var buildingImages = {
         "Boyd Orr": boydOrrImage,
-        "Library": libraryImage
+        "Library": libraryImage,
+        "St Andrews": stAndrewsImage
     }
 
     // Function to convert number of people to hex value
@@ -38,8 +51,20 @@ function Map()
         <div className="Card layout-map">
             <div className='Map'>
                 <img src={mapBackground} className="map-image" ></img>
-                <Building intensity={0.5} image={buildingImages["Boyd Orr"]}></Building>
-                <Building intensity={0.5} image={buildingImages["Library"]}></Building>
+                <Building intensity={0.5} image={boydOrrImage}></Building>
+                <Building intensity={0.5} image={libraryImage}></Building>
+                <Building intensity={0.5} image={stAndrewsImage}></Building>
+                <Building intensity={0.5} image={kelvinImage}></Building>
+                <Building intensity={0.5} image={josephBlackImage}></Building>
+                <Building intensity={0.5} image={qmuImage}></Building>
+                <Building intensity={0.5} image={alwynImage}></Building>
+                <Building intensity={0.5} image={hiveImage}></Building>
+                <Building intensity={0.5} image={guuImage}></Building>
+                <Building intensity={0.5} image={wolfsonImage}></Building>
+                <Building intensity={0.5} image={mainImage}></Building>
+                <Building intensity={0.5} image={adamSmithImage}></Building>
+                <Building intensity={0.7} image={jwsImage}></Building>
+                <Building intensity={0} image={kelvingroveImage}></Building>
             </div>
         </div>
     );
