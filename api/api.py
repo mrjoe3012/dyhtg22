1c9306fc16c5fc88c2eda84eaa7460e10fa7abcc
Joseph Agrane <2663460A@student.gla.ac.uk>
2022-10-15 19:07:39 +0100

    added heatmap api

--- api/api.py
+++ api/api.py
@@ -6,6 +6,11 @@ from Utils import get_security_logs
 
 app = Flask(__name__)
 
+@app.route("/get-heatmap")
+def route_get_heatmap():
+    hm = Utils.get_heatmap_values(Utils.get_security_logs(Utils.DATABASE_FILENAME))
+    return jsonify(hm)
+
 @app.route("/get-security-logs")
 def route_get_security_logs():
     security_log = get_security_logs(Utils.DATABASE_FILENAME)
