53d5b6b922228d761acad57f285c66313a3ed4a6
Joseph Agrane <2663460A@student.gla.ac.uk>
2022-10-15 23:36:36 +0100

    added cors headers

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 import json
 import Utils
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 
 from Utils import get_security_logs
 
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route("/get-heatmap")
 def route_get_heatmap():
     hm = Utils.get_heatmap_values(Utils.get_security_logs(Utils.DATABASE_FILENAME))
-    return jsonify(hm)
+    resp = Response(jsonify(hm))
+    resp.headers["Access-Control-Allow-Origin"] = "*"
+    return resp
 
 @app.route("/get-security-logs")
 def route_get_security_logs():
