bfe11ef145c5d5f87fe978fd5ae0f4ba86b7040f
Joseph Agrane <2663460A@student.gla.ac.uk>
2022-10-15 13:18:35 +0100

    #21: added time checking functions

--- api/Utils/DataHandling.py
+++ api/Utils/DataHandling.py
@@ -32,17 +32,40 @@ def get_heatmap_values(security_logs):
 
     return occurrence_dict
 
+# returns a time range where the exit is always greater than entry:
+# e.g.
+# 0900-1200 -> 0900-1200 (no change)
+# 2200-0200 -> 2200-2600 (0200 -> 2400 + 0200)
+def make_time_friendly(time_range):
+    times = [int(x) for x in time_range.split("-")]
+    if times[1] > times[0]:
+        return time_range
+    else:
+        times[1] += 2400
+        new_time_range = "{0}-{1}".format(times[0],times[1])
+        return new_time_range
+
 # returns true if the two time ranges overlap
 # e.g. 
 # check_time_overlap("2100-2200", "2150-2220") = True
 # check_time_overlap("2350-0100", "0010-0200") = True
 # check_time_overlap("1600-1800", "1900-2100") = False
 def check_time_overlap(time_range_1, time_range_2):
-    pass
+    # revert mod 2400 behaviour of time
+    time_range_1_friendly, time_range_2_friendly = make_time_friendly(time_range_1), make_time_friendly(time_range_2)
+    times_1, times_2 = [int(x) for x in time_range_1.split("-")], [int(x) for x in time_range_2.split("-")]
+    
+    # return time_range_1 ∩ time_range_2 ≠ Ø
+    return (times_1[0] >= times_2[0] and times_1[0] <= times_2[1]) or (times_2[0] >= times_1[0] and times_2[0] <= times_2[1])
 
 # returns true if check_time is within the time_range
 # e.g.
 # check_time_in_range("0900-1700", "1000-1200") = True
 # check_time_in_range("2200-0200", "0100-0300") =
 def check_time_in_range(time_range, check_time):
-    pass
+    # revert mod 2400 behaviour of time
+    time_range_friendly, check_time_friendly = make_time_friendly(time_range), make_time_friendly(check_time)
+    times_range_ints, check_time_ints = [int(x) for x in time_range_friendly.split("-")], [int(x) for x in check_time_friendly.split("-")]
+
+    # return check_time ⊆ time_range
+    return (check_time_ints[0] >= time_range_ints[0] and check_time_ints[0] <= time_range_ints[1]) and (check_time_ints[1] >= time_range_ints[0] and check_times_ints[1] <= time_range_ints[1])
