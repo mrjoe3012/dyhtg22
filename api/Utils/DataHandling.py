c6184f7988750d1ab54fff2a6c0ed5919c6fbd1b
Joseph Agrane <2663460A@student.gla.ac.uk>
2022-10-15 15:08:18 +0100

    #21: added time checking functions and tests for them

--- api/Utils/DataHandling.py
+++ api/Utils/DataHandling.py
@@ -32,18 +32,12 @@ def get_heatmap_values(security_logs):
 
     return occurrence_dict
 
-# returns a time range where the exit is always greater than entry:
-# e.g.
-# 0900-1200 -> 0900-1200 (no change)
-# 2200-0200 -> 2200-2600 (0200 -> 2400 + 0200)
-def make_time_friendly(time_range):
-    times = [int(x) for x in time_range.split("-")]
-    if times[1] > times[0]:
-        return time_range
-    else:
-        times[1] += 2400
-        new_time_range = "{0}-{1}".format(times[0],times[1])
-        return new_time_range
+# returns the time difference in minutes between
+# time2 and time 1
+def time_difference(time1, time2):
+    if time2 < time1:
+        time2 += 2400
+    return time2 - time1
 
 # returns true if the two time ranges overlap
 # e.g. 
@@ -51,25 +45,20 @@ def make_time_friendly(time_range):
 # check_time_overlap("2350-0100", "0010-0200") = True
 # check_time_overlap("1600-1800", "1900-2100") = False
 def check_time_overlap(time_range_1, time_range_2):
-    # revert mod 2400 behaviour of time
-    time_range_1_friendly, time_range_2_friendly = make_time_friendly(time_range_1), make_time_friendly(time_range_2)
-    times_1, times_2 = [int(x) for x in time_range_1.split("-")], [int(x) for x in time_range_2.split("-")]
-    
-    # return time_range_1 ∩ time_range_2 ≠ Ø
-    return (times_1[0] >= times_2[0] and times_1[0] <= times_2[1]) or (times_2[0] >= times_1[0] and times_2[0] <= times_2[1])
+    time1_ints, time2_ints = [int(x) for x in time_range_1.split("-")], [int(x) for x in time_range_2.split("-")]
+    time1_difference = time_difference(time1_ints[0], time1_ints[1])
+    time2_difference = time_difference(time2_ints[0], time2_ints[1])
+    return (time_difference(time1_ints[0], time2_ints[0]) <= time1_difference or time_difference(time1_ints[0], time2_ints[1]) <= time1_difference) or (time_difference(time2_ints[0], time1_ints[0]) <= time2_difference or time_difference(time2_ints[0], time1_ints[1]) <= time2_difference)
 
 # returns true if check_time is within the time_range
 # e.g.
 # check_time_in_range("0900-1700", "1000-1200") = True
 # check_time_in_range("2200-0200", "0100-0300") =
 def check_time_in_range(time_range, check_time):
-    # revert mod 2400 behaviour of time
-    time_range_friendly, check_time_friendly = make_time_friendly(time_range), make_time_friendly(check_time)
-    times_range_ints, check_time_ints = [int(x) for x in time_range_friendly.split("-")], [int(x) for x in check_time_friendly.split("-")]
-
-    # return check_time ⊆ time_range
-    return (check_time_ints[0] >= time_range_ints[0] and check_time_ints[0] <= time_range_ints[1]) and (check_time_ints[1] >= time_range_ints[0] and check_times_ints[1] <= time_range_ints[1])
-
+    if time_range == "0000-0000": return True
+    time1_ints, time2_ints = [int(x) for x in time_range.split("-")], [int(x) for x in check_time.split("-")]
+    time1_difference = time_difference(time1_ints[0], time1_ints[1])
+    return (time_difference(time1_ints[0], time2_ints[0]) <= time1_difference and time_difference(time1_ints[0], time2_ints[1]) <= time1_difference)
 def detect_security_breaches(security_logs):
     # go through security logs
     # find out when a building is visited out of hours
--- api/test_dataHandling.py
+++ api/test_dataHandling.py
@@ -6,15 +6,26 @@ class TestDataHandling(unittest.TestCase):
         security_breaches = Utils.detect_security_breaches(Utils.get_security_logs(Utils.DATABASE_FILENAME))
         self.assertTrue("Abdul Murphy" in security_breaches and ("Boyd Orr Building", "2208-2219") in security_breaches["Abdul Murphy"])
 
-    def test_time_check(self):
-        self.assertTrue(Utils.time_check("0900-1700", "2000-2015")) # out of hours
-        self.assertTrue(Utils.time_check("0900-1700", "0900-1200")) # right on time
-        self.assertTrue(Utils.time_check("0900-1700", "1845-1000")) # out of hours, slept in
-        self.assertTrue(Utils.time_check("0900-1700", "0945-1030")) # in hours
-        self.assertTrue(Utils.time_check("2100-0200", "0120-0200")) # kicked out
-        self.assertTrue(Utils.time_check("2100-0200", "2045-2120")) # out of hours
-        self.assertTrue(Utils.time_check("0000-0000", "1220-1330")) # control - no hours
-        self.assertTrue(Utils.time_check("0000-0000", "0345-0445")) # control - no hours
+    def test_friendly_time(self):
+        pass
+
+    def test_check_time_in_range(self):
+        self.assertFalse(Utils.check_time_in_range("0900-1700", "2000-2015")) # out of hours
+        self.assertTrue(Utils.check_time_in_range("0900-1700", "0900-1200")) # right on time
+        self.assertFalse(Utils.check_time_in_range("0900-1700", "1845-1000")) # out of hours, slept in
+        self.assertTrue(Utils.check_time_in_range("0900-1700", "0945-1030")) # in hours
+        self.assertTrue(Utils.check_time_in_range("2100-0200", "0120-0200")) # kicked out
+        self.assertFalse(Utils.check_time_in_range("2100-0200", "2045-2120")) # out of hours
+        self.assertTrue(Utils.check_time_in_range("0000-0000", "1220-1330")) # control - no hours
+        self.assertTrue(Utils.check_time_in_range("0000-0000", "0345-0445")) # control - no hours
+
+    def test_check_time_overlap(self):
+        self.assertTrue(Utils.check_time_overlap("0900-1200", "0900-1200")) # t
+        self.assertFalse(Utils.check_time_overlap("0900-1200", "1201-1220")) # f
+        self.assertTrue(Utils.check_time_overlap("2300-0200", "0145-0300")) # t
+        self.assertFalse(Utils.check_time_overlap("2345-0100", "2250-2330")) # f
+        self.assertTrue(Utils.check_time_overlap("1700-2200", "1730-2300")) # t
+        self.assertFalse(Utils.check_time_overlap("0200-0300", "1500-1650")) # f
 
 if __name__ == "__main__":
     unittest.main()
