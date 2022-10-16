b1c5562777a1a433702d637ef14f18cffc89d208
Jamie Robb <jamierobb321@gmail.com>
2022-10-16 04:34:31 +0100

    Output graph from api

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 import './Card.css'
 import './Layout.css'
 
-
 function Graph() {
+
+    const [graphImage, setGraphImage] = useState();
+
+    function generateGraph() {
+        setGraphImage("http://127.0.0.1:5000/get-interaction-graph?students=")
+    }
+
+    console.log(typeof(graphImage));
+
     return (
         <div className="Card layout-graph">
             <h1>Graph</h1>
+            <button onClick={generateGraph}>Generate</button>
+            <img src={graphImage}></img>
         </div>
     )
 }
